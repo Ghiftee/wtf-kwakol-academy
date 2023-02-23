e7bb1ae2fa86c55aee308e627aa2187d0d0e1811
Ghiftee <gigibetine@gmail.com>
2023-02-23 16:52:36 +0100

    Create start page pending image

---
+++ src/components/home/Start.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+function Start() {
+  return (
+    <div className='get-started'>
+        <button className='start-btn'>Get Started</button>
+    </div>
+  )
+}
+
+export default Start
