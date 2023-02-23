d82d5d8ea74c1b903bce1ec1d78c028a5d94e47f
Ghiftee <gigibetine@gmail.com>
2023-02-23 11:03:36 +0100

    Set some general functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+// import Home from './components/home/Home';
+// import Contact from './components/contact/Contact';
+// import About from './components/about/About'
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+     
     </div>
   );
 }
