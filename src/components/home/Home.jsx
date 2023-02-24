92bdf96b7a8d9f0fc6031c05c72c0604b6db815f
Ghiftee <gigibetine@gmail.com>
2023-02-24 16:13:13 +0100

    Add new links

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,29 +1,35 @@
-import React from 'react';
 import Kwakol from '../../images/header-img.png';
 import './Home.css'
+import Study from './Study';
+import Start from './Start';
+import Reviews from './Reviews';
 
 function Home() {
   return (
-    <div className='hero-section' id='home'>
-      <div className='hero-section-lhs'>
-        <h1>
-          Learn How To Trade <br />
-          <span className='gold-word'>Forex</span> With Kwakol<br />
-          Academy
-        </h1>
-        <p>
-          Become a profitable trader with <br /> 
-          our comprehensive forex trading <br /> 
-          courses, for all levels.
-        </p>
-        <button>Enroll Now</button>
+    <div id='home'>
+      <div className='hero-section'>
+        <div className='hero-section-lhs'>
+          <h1>
+            Learn How To Trade <br />
+            <span className='gold-word'>Forex</span> With Kwakol<br />
+            Academy
+          </h1>
+          <p>
+            Become a profitable trader with <br /> 
+            our comprehensive forex trading <br /> 
+            courses, for all levels.
+          </p>
+          <button>Enroll Now</button>
+        </div>
+        <div>
+          <img src={Kwakol} alt='kwakol-people' className='hero-section-img'/>
+        </div>
       </div>
-      <div>
-        <img src={Kwakol} alt='kwakol-people' className='hero-section-img'/>
-      </div>
+      <Study />
+      <Start />
+      <Reviews />
     </div>
-
   )
 }
 
-export default Home
+export default Home;
