6ac699cd5572f75929f2b0f03f43b13f71a6a4c3
Ghiftee <gigibetine@gmail.com>
2023-02-28 10:15:42 +0100

    Fix hash routing problem

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter> 
+    <BrowserRouter basename='/wtf-kwakol-academy'> 
      <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
 // Note:
 // Browser router is used because it is a browser app, if it was mobile, mobile router would be used
 // App is wrapped in browser router because it covers the entire app
 // However, since we are deploying using gh-pages, to avoid 404 error, we would use hash router
+//The issue with hash router is that it gives a hash in the app path e.g localhost:3000/#/contact
+//To remove the hash so that it becomes localhost:3000/contact, we use browser router
+// According to chatGPT, using basename in the browserrouter component as used above should fix the 404 error
