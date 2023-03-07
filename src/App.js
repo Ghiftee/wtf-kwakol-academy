d74bd13d4a28565f9902255f8e756d2c497558a3
Ghiftee <gigibetine@gmail.com>
2023-03-07 13:57:46 +0100

    Create membership page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './components/pages/home/Home';
 import About from './components/pages/about/About';
 import Contact from './components/pages/contact/Contact';
+import Members from './components/pages/membership/Members';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/membership' element={<Members />} />
       </Routes>
     </div>
   );
