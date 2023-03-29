ea4a73443c0edefb5628019c17508b317678383c
Ghiftee <gigibetine@gmail.com>
2023-03-29 23:37:25 +0100

    Redesign contact page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar(props) {
         navigate('/gettheapp')
     }
   return (
-    <div style={{backgroundColor: props.backgroundColor || 'var(--primaryBackground)', padding: props.padding || '32px 0'}}>
+    <div>
         <nav className='navbar'>
             <section className='navLhs'>
                 <img src={Logo} alt="logo" />
@@ -23,7 +23,7 @@ function Navbar(props) {
                     {/* <NavLink to='/' className={isActive ? 'active-link mr' : 'inactive-link mr'} style={({ isActive }) => ({color: isActive? 'var(--primaryColor)' : 'black'})} className='mr'>Home</NavLink> */}
                     <NavLink to='/' style={({ isActive }) => ({color: isActive? 'var(--primaryColor)' : 'black'})} className='mr'>Home</NavLink>
                     <NavLink to='/about' style={({ isActive }) => ({color: isActive? 'var(--primaryColor)' : 'black'})} className='mr'>About Us</NavLink>
-                    <NavLink to='/contact' style={({ isActive }) => ({color: isActive? 'var(--primaryColor)' : 'black'})} className='mr'>Contact</NavLink>
+                    <NavLink to='/contact' style={({ isActive }) => ({color: isActive? 'var(--primaryColor)' : 'black'})} className='mr'>Contact Us</NavLink>
                     <NavLink to='/membership' style={({ isActive }) => ({color: isActive? 'var(--primaryColor)' : 'black'})}>Become a member</NavLink>            
                 </ul>
                 <button onClick={navigateToLoginPage} className='loginBtn'>Login</button>
--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,55 +1,70 @@
 import React from 'react';
 import Navbar from '../../navbar/Navbar';
 import './Contact.css';
+import CustomerCare from '../../../images/contact-person.png';
 import Footer from '../home/Footer';
 
 function Contact() {
   return (
     <>
-      <div className='contact-section'>
-        <Navbar backgroundColor='#c2eecc' padding='10px 0'/>
-        <div className='contact-puzzle'>
-          <section className='contact-puzzle-text'>
-            <h2 className='contact-puzzle-text-h2'>Contact Us Now</h2>
-            <p className='contact-puzzle-text-p-1'>
-              If you have any questions about our services, would like to request a brochure <br/>
-              or request a call back please use the form and we will get back to you as soon <br/>
-              as possible.
+    <div className='kwakol-contact'>
+      <Navbar/>
+      <div className='contact-page'>
+        <p className='contact-page-title'>Contact Us</p>
+        <img src={CustomerCare} alt='customer care personnel' className='contact-img'/>
+        <section className='contacts-section'>
+          <section>
+            <p className='address-heading'>Address</p>
+            <p className='address-text'>
+              1st floor, Mall 169, Adetokunbo<br/> 
+              Ademola Crescent, Wuse II,<br/>
+              Abuja, Nigeria
             </p>
-            <section>
-              <h3 className='contact-puzzle-text-h3'>Our Company</h3>
-              <p className='contact-puzzle-text-p-2 mb'>Kwakol academy</p>
-            </section>
-            <h3 className='contact-puzzle-text-h3'>Contacts</h3>
-            <p className='contact-puzzle-text-p-2'>08023399491</p>
           </section>
-          <span className='contact-puzzle-span'>
-            1st floor, Mall 169, <br/>
-            Adetokunbo Ademola <br/>
-            Crescent, Wuse II, Abuja,<br/>
-            Nigeria
-          </span>
-        </div>
-        <section>
-          <p className='contact-form-p'>Or you can contact us via filling the form</p>
-          <form className='contact-form'>
-            <label for='name' className='contact-form-label'>Your name</label>
-            <input type='text' className='contact-form-input'/>
-            <label for='email' className='contact-form-label'>Your Email</label>
-            <input type='email' className='contact-form-input'/>
-            <label for='p-number' className='contact-form-label'>Your Phone Number</label>
-            <input type='number' className='contact-form-input'/>
-            <label for='subject' className='contact-form-label'>Subject</label>
-            <input type='text' className='contact-form-input'/>
-            <label for='msg' className='contact-form-label'>Your message</label>
-            <textarea name='msg' className='contact-form-textArea'></textarea>
-            <button className='contact-form-btn'>Send a message</button>
-          </form>
+          <section>
+            <p className='contact-heading'>Contact</p>
+            <p className='contact-details'>
+              Mobile: 08023399491
+            </p>
+          </section>
+          <section>
+            <p className='operations'>Hour Operation</p>
+            <p className='operations-hour'>
+              Monday-Friday;<br/>
+              09:00-17:00
+            </p>
+          </section>
         </section>
+        <p className='intersection'>
+          If you have any questions about our services, would like to request a <br/>
+          brochure or request a call back please use the form and we will get back to <br/>
+          you as soon as possible.
+        </p>
+        <form className='contact-form'>
+          <div className='form-first-section'>
+            <div className='input-section'>
+              <label for='name' className='contact-form-top-label'>Name*</label>
+              <input type='text' className='contact-form-input'/>
+            </div>
+            <div className='input-section'>
+              <label for='email' className='contact-form-top-label'>Email*</label>
+              <input type='email' className='contact-form-input'/>
+            </div>
+          </div>
+          <div className='form-second-section'>
+            <label for='subject' className='contact-form-bottom-label'>Subject*</label>
+            <input type='text' className='contact-form-subject-input'/>
+            <label for='msg' className='contact-form-bottom-label'>Message*</label>
+            <textarea name='msg' className='contact-form-textArea'></textarea>
+          </div>
+          <button className='contact-form-btn'>Send</button>
+        </form>
       </div>
-      <Footer />
+    </div>
+    <Footer />
     </>
   )
 }
 
 export default Contact;
+
