d779f09585f26e957c03f5508b2136691ea8b59c
Ghiftee <gigibetine@gmail.com>
2023-03-30 09:49:38 +0100

    Add new pages to home

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -2,37 +2,41 @@ import Kwakol from '../../../images/header-img.png';
 import './Home.css'
 import Navbar from '../../navbar/Navbar';
 import Study from './Study';
-import Start from './Start';
+import PopularCourses from './PopularCourses';
 import Reviews from './Reviews';
+import GetApp from './GetApp';
 import Footer from './Footer';
 
 function Home() {
   return (
-    <div>
-      <Navbar />
-      <div className='hero-section'>
-        <div className='hero-section-lhs'>
-          <h1>
-            Learn How To Trade <br />
-            <span className='gold-word'>Forex</span> With Kwakol<br />
-            Academy
-          </h1>
-          <p>
-            Become a profitable trader with <br /> 
-            our comprehensive forex trading <br /> 
-            courses, for all levels.
-          </p>
-          <button>Get Started</button>
+    <>
+      <div className='home-page'>
+        <Navbar />
+        <div className='hero-section'>
+          <div className='hero-section-lhs'>
+            <h1>
+              Learn How To Trade <br />
+              <span className='gold-word'>Forex</span> With Kwakol<br />
+              Academy
+            </h1>
+            <p>
+              Become a profitable trader with <br /> 
+              our comprehensive forex trading <br /> 
+              courses, for all levels.
+            </p>
+            <button>Get Started</button>
+          </div>
+          <div>
+            <img src={Kwakol} alt='kwakol-people' className='hero-section-img'/>
+          </div>
         </div>
-        <div>
-          <img src={Kwakol} alt='kwakol-people' className='hero-section-img'/>
-        </div>
+        <Study />
+        <PopularCourses />
+        <Reviews />
+        <GetApp />
+        <Footer />
       </div>
-      <Study />
-      <Start />
-      <Reviews />
-      <Footer />
-    </div>
+    </>
   )
 }
 
